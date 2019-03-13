606480335694907d1e03dd7547064bb73a0c9c60
Aleksandr_Kolenov <Aleksandr_Kolenov@epam.com>
2019-03-13 14:53:07 +0300

    WindowsService: Add Reporter

--- WindowsService/Service.cs
+++ WindowsService/Service.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Reporter;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,17 +13,49 @@ namespace WindowsService
 {
     public partial class Service : ServiceBase
     {
+        EventLog Log;
+        TradingReporter TradingReporter;
+
         public Service()
         {
             InitializeComponent();
+            Log = new EventLog();
+            Log.Source = ServiceName;
+            Log.Log = "Application";
+            Log.BeginInit();
+            if (!EventLog.SourceExists(Log.Source))
+            {
+                EventLog.CreateEventSource(Log.Source, Log.Log);
+            }
+            Log.EndInit();
+            TradingReporterConfiguration config = new TradingReporterConfiguration();
+            config.UpdateFromAppConfig();
+
+            TradingReporter = new TradingReporter(config);
         }
 
         protected override void OnStart(string[] args)
         {
+            Log.WriteEntry("Started");
+            TradingReporter.OnStart();
         }
 
         protected override void OnStop()
         {
+            Log.WriteEntry("Stopped");
+            TradingReporter.OnStop();
+        }
+
+        protected override void OnPause()
+        {
+            Log.WriteEntry("Paused");
+            TradingReporter.OnPause();
+        }
+
+        protected override void OnContinue()
+        {
+            Log.WriteEntry("Continue");
+            TradingReporter.OnContinue();
         }
     }
 }
