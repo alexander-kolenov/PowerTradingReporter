687a1b8eea6b006ba713da2d5b785e343320d82a
Aleksandr_Kolenov <Aleksandr_Kolenov@epam.com>
2019-03-13 18:34:50 +0300

    Utils: Add ILogger

---
+++ Utils/Logger/ILogger.cs
@@ -0,0 +1,16 @@
+﻿namespace Utils.Logger
+{
+    public interface ILogger
+    {
+        void Log(LogLevel level, string message);
+    }
+
+    public enum LogLevel
+    {
+        None,
+        Debug,
+        Info,
+        Warning,
+        Error,
+    }
+}
