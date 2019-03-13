6c1544c5e6043c4e5f23c60ae6674e0cae4775fb
Aleksandr_Kolenov <Aleksandr_Kolenov@epam.com>
2019-03-13 10:25:22 +0300

    Utils: Add CsvWriter

---
+++ Utils/Csv/CsvData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Utils.Csv
+{
+    public class CsvData
+    {
+        public string[] Headers { get; set; }
+        public List<string[]> Rows { get; set; }
+
+
+        public CsvData(string[] headers)
+        {
+            Headers = headers;
+            Rows = new List<string[]>();
+        }
+
+        public void AddRow(string[] row)
+        {
+            Rows.Add(row);
+        }
+    }
+}
