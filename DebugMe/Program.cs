d023ed2e817fa3b8bd79c7044a0d35a532ca014b
Aleksandr_Kolenov <Aleksandr_Kolenov@epam.com>
2019-03-13 18:36:38 +0300

    DebugMe: Add Test2

--- DebugMe/Program.cs
+++ DebugMe/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Reporter;
 using System;
 using System.Diagnostics;
+using Utils.Logger;
 
 namespace DebugMe
 {
@@ -8,21 +9,60 @@ namespace DebugMe
     {
         static void Main(string[] args)
         {
+            Test2();
+        }
+
+        private static void Test2()
+        {
             TradingReporterConfiguration c = new TradingReporterConfiguration();
             c.UpdateFromAppConfig();
 
-            TradingReporter reporter = new TradingReporter(c);
+            TradingReporter reporter = new TradingReporter(c, new DebugLogger());
+
+            while (true)
+            {
+                var key = Console.ReadKey().KeyChar;
+                switch (key)
+                {
+                    case 's':
+                        Console.WriteLine("Start");
+                        reporter.OnStart();
+                        break;
+                    case 'x':
+                        Console.WriteLine("Stop");
+                        reporter.OnStop();
+                        break;
+                    case 'p':
+                        Console.WriteLine("Pause");
+                        reporter.OnPause();
+                        break;
+                    case 'c':
+                        Console.WriteLine("Continue");
+                        reporter.OnContinue();
+                        break;
+                }
+
+            }
+            
+        }
+
+        private static void Test1()
+        {
+            TradingReporterConfiguration c = new TradingReporterConfiguration();
+            c.UpdateFromAppConfig();
+
+            TradingReporter reporter = new TradingReporter(c, new DebugLogger());
 
             DateTime dt = DateTime.Now;
 
             for (int i = 0; i < 10; i++)
             {
-                dt = dt.AddMonths(1);
+                dt = dt.AddHours(1);
                 try
                 {
                     reporter.MakeReport(dt);
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
                     Debug.WriteLine($"{dt}:  {ex.Message}  -- {ex.StackTrace}");
                 }
