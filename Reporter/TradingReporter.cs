2cc9e08d6a02f9311f520c5c88b8a37f6f362430
Aleksandr_Kolenov <Aleksandr_Kolenov@epam.com>
2019-03-14 10:45:04 +0300

    Reporter: Fix TradingReporterConfiguration

--- Reporter/TradingReporter.cs
+++ Reporter/TradingReporter.cs
@@ -12,36 +12,37 @@ namespace Reporter
         private readonly TradingReporterConfiguration _config;
         private readonly ILogger _logger;
         private readonly Timer _timer;
-        private volatile Task _currentTask;
+        private volatile Task _executionTask;
 
         public TradingReporter(TradingReporterConfiguration config, ILogger logger)
         {
             _config = config;
             _logger = logger;
+            _executionTask = new Task(() => { }); // Set _executionTask != null
+            _executionTask.RunSynchronously(); // Make _executionTask.IsCompleted = true
             _timer = new Timer(new TimerCallback(TimerProc));
-            _currentTask = Task.Run(() => { });
         }
 
-        public void MakeReportSafe(DateTime localTime)
+        public void MakeReportSafe(DateTime utcTime)
         {
             try
             {
-                _logger.Log(LogLevel.Debug, $"MakeReport({localTime})");
-                MakeReport(localTime);
+                MakeReport(utcTime);
+                _logger.Log(LogLevel.Debug, $"Make Report({utcTime})");
             }
             catch (Exception ex)
             {
-                _logger.Log(LogLevel.Error, $"Exception: {ex.Message}");
+                _logger.Log(LogLevel.Error, $"Exception: {ex.Message} {ex.StackTrace}");
             }
         }
 
-        public void MakeReport(DateTime localTime)
+        public void MakeReport(DateTime utcTime)
         {
             DataAcquisition da = new DataAcquisition();
-            AggregatedData ad = da.GetAggregatedTrades(localTime);
+            AggregatedData ad = da.GetAggregatedTrades(utcTime);
 
             ReportBuilder rb = new ReportBuilder();
-            string reportFileName = Path.Combine(_config.ReportingDirrectory, rb.GetCsvReportFileName(localTime));
+            string reportFileName = Path.Combine(_config.ReportingDirrectory, rb.GetCsvReportFileName(utcTime));
             CsvData csvData = rb.CreateCsvData(ad);
 
             CsvWriter w = new CsvWriter();
@@ -57,7 +58,7 @@ namespace Reporter
         public void OnStop()
         {
             _timer.Change(Timeout.Infinite, Timeout.Infinite);
-            Task.WaitAll(new[] { _currentTask });
+            Task.WaitAll(new[] { _executionTask });
         }
 
         public void OnContinue() => _timer.Change(0, GetTimerInterval());
@@ -71,15 +72,15 @@ namespace Reporter
         private int GetTimerInterval()
         {
             _config.UpdateFromAppConfig();
-            return _config.ReportingIntervalInMinutes * 60 * 1000;
+            return (int)_config.ReportingInterval.TotalMilliseconds;
         }
 
         private void TimerProc(object state)
         {
-            if (_currentTask.IsCompleted)
-                _currentTask = Task.Run(() => MakeReportSafe(DateTime.Now));
+            if (_executionTask.IsCompleted)
+                _executionTask = Task.Run(() => MakeReportSafe(DateTime.UtcNow));
             else
-                _logger.Log(LogLevel.Warning, $"Skip report at {DateTime.Now}. Reason: previous report is running");
+                _logger.Log(LogLevel.Warning, $"Skip report at {DateTime.UtcNow:u}. Reason: previous report is running");
         }
 
         #endregion
--- Reporter/TradingReporterConfiguration.cs
+++ Reporter/TradingReporterConfiguration.cs
@@ -5,18 +5,22 @@ namespace Reporter
 {
     public class TradingReporterConfiguration
     {
-        public string ReportingDirrectory { get; set; } = AppDomain.CurrentDomain.BaseDirectory;
-        public int ReportingIntervalInMinutes { get; set; } = 5;
+        public static TimeSpan DefaultReportingInterval => TimeSpan.FromMinutes(5);
+        public static string DefaultReportingDirrectory => AppDomain.CurrentDomain.BaseDirectory;
+
+
+        public string ReportingDirrectory { get; set; } = DefaultReportingDirrectory;
+        public TimeSpan ReportingInterval { get; set; } = DefaultReportingInterval;
 
         public void UpdateFromAppConfig()
         {
             string str;
 
             str = ConfigurationManager.AppSettings["ReportingDirrectory"];
-            ReportingDirrectory = !String.IsNullOrWhiteSpace(str) ? str : AppDomain.CurrentDomain.BaseDirectory;
+            ReportingDirrectory = !String.IsNullOrWhiteSpace(str) ? str : DefaultReportingDirrectory;
 
             str = ConfigurationManager.AppSettings["ReportingIntervalInMinutes"];
-            ReportingIntervalInMinutes = (str != null && int.TryParse(str, out int interval) && interval > 0) ? interval : 5;
+            ReportingInterval = (str != null && int.TryParse(str, out int interval) && interval > 0) ? TimeSpan.FromMinutes(interval) : DefaultReportingInterval;
 
         }
     }
