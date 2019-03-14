92aaead520f59864d275a588c60a4488cc51e902
Aleksandr_Kolenov <Aleksandr_Kolenov@epam.com>
2019-03-14 10:43:41 +0300

    Utils: Add ConsoleLogger

---
+++ Utils/Logger/ConsoleLogger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Utils.Logger
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void Log(LogLevel level, string message)
+        {
+            Console.WriteLine($"{DateTime.UtcNow:u} {level}: {message}");
+        }
+    }
+}
