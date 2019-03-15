354610060e838fc90e4cc741c491c89594342a86
Aleksandr_Kolenov <Aleksandr_Kolenov@epam.com>
2019-03-15 12:58:48 +0300

    UnitTestsForReporter: Add AgregateTrades test

--- UnitTestsForReporter/DataAcquisitionTests.cs
+++ UnitTestsForReporter/DataAcquisitionTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Reporter;
+using TradingPlatform;
 
 namespace UnitTestsForReporter
 {
@@ -43,5 +45,25 @@ namespace UnitTestsForReporter
             DateTime tradingDay = da.GetTradingDay(now, si);
             Assert.AreEqual(tradingDay, new DateTime(2000, 5, 7));
         }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var da = new DataAcquisition();
+            SessionInfo si = new SessionInfo();
+            si.SessionStart = TimeSpan.FromHours(15);
+            DateTime now = new DateTime(2000, 5, 6, 15, 0, 0);
+            List<Trade> trades = new List<Trade>();
+            trades.Add(Trade.Create(now, 1));
+            trades.Add(Trade.Create(now, 2));
+            trades.Add(Trade.Create(now, 3));
+
+            AggregatedData at = da.AgregateTrades(now, si, trades);
+
+            Assert.AreEqual(at.TradingDate, now);
+            Assert.AreEqual(at.SessionStart, si.SessionStart);
+            Assert.AreEqual(at.Volumes.Length, 24);
+        }
+
     }
 }
